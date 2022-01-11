652fb3fd0597ea05088902c2ea8ff0e49d0a27d4
cogito1016 <cogito1016@naver.com>
2022-01-11 19:18:48 +0900

    chore: 원격저장소 재업데이트 -git rm -r --cached -git add .

--- index.ts
+++
@@ -1,7 +0,0 @@
-const sayHi = (name:string, age:number, gender:string):string => {
-    return `hello ${name} you are ${age} years old and you are ${gender}!`;
-}
-
-console.log(sayHi("Jayden", 14, "male"));
-
-export {}; //Typescript법칙, 이 파일이 모듈이 된다는걸 이해할 수 있도록 명시
