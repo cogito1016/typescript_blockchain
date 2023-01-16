333485625014c2b032ef4355592c9983d8402e68
김재형 <cogito1016@naver.com>
2023-01-16 22:36:36 +0900

    feat: Arr & Tuple

---
+++ basic/tuple.ts
@@ -0,0 +1,8 @@
+let person1: [string, number] = ['monica',123];
+console.log(person1);
+
+// let person2: [string, number] = ['kim',456,123];
+// occured error
+
+// let person3: [string, number] = [123,'kiz'];
+// occured error
