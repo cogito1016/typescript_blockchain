552fbffb64bb6871a00a060d7644de42ce77db12
김재형 <cogito1016@naver.com>
2023-01-22 21:38:18 +0900

    docs: Number Sorter 예제

--- problem/lv4/number-sorter.ts
+++ problem/lv4/number-sorter.ts
@@ -1,5 +1,11 @@
 import { Common } from "../common"
-
+/**
+ * 예제 입력 1 
+    10 5
+    1 10 4 9 2 3 8 5 7 6
+    예제 출력 1 
+    1 4 2 3
+ */
 export const NumberSorter = () => {
     const input : string[] = Common();
 
