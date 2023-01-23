0fbea47d17d03e808d3d1785d2939df7245a88af
김재형 <cogito1016@naver.com>
2023-01-24 00:15:54 +0900

    ?? - -..

--- problem/lv9/matrix-adder.ts
+++ problem/lv9/matrix-adder.ts
@@ -4,7 +4,7 @@ export const MatrixAdder = () => {
     const readline = READLINE();
 
     function solution(input) {
-        const [x, y] = input[0].split(' ').map((str: string) => parseInt(str));
+        const [y, x] = input[0].split(' ').map((str: string) => parseInt(str));
 
         input.shift()
 
@@ -20,4 +20,52 @@ export const MatrixAdder = () => {
     }
 
     readline(solution);
-}  
+}
+
+/**
+1.
+3 3
+1 1 1
+2 2 2
+0 1 0
+3 3 3
+4 4 4 
+5 5 100
+
+4 4 4
+6 6 6 
+5 6 100
+
+2.
+3 4
+1 1 1 1
+2 2 2 2
+3 3 3 3
+4 4 4 4
+5 5 5 5
+6 6 6 6
+
+5 5 5 5 
+7 7 7 7
+9 9 9 9
+
+3.
+1 1
+1
+2
+
+3
+
+4.
+3 1
+1
+2
+3
+4
+5
+6
+
+5
+7
+9
+ */
