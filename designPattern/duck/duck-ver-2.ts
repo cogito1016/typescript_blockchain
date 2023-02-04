27030e5431e0b18474513d0d186a38faadbe2c6d
김재형 <cogito1016@naver.com>
2023-02-05 01:36:33 +0900

    refactor: interface로 재정의가 필요한 부분만 추출

---
+++ designPattern/duck/duck-ver-2.ts
@@ -0,0 +1,27 @@
+
+class Duck_2 {
+    display(): void {
+        console.log("귀여운 노란 덕")
+    }
+
+    swim(): void {
+        console.log("어푸푸")
+    }
+}
+
+interface quackAble {
+    quack(): void;
+}
+
+interface flyAble {
+    fly(): void
+}
+
+class MallardDuck_1 extends Duck_2 implements quackAble, flyAble {
+    quack(): void {
+        console.log("꽥")
+    }
+    fly(): void {
+        console.log("난 날아")
+    }
+}
