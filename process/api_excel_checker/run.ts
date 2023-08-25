09e177da608abaa01ac058d13acbd7e50b0334f5
김재형 <cogito1016@naver.com>
2023-08-25 15:29:05 +0900

    refactor: solution 파라미터화

--- process/api_excel_checker/run.ts
+++ process/api_excel_checker/run.ts
@@ -1,6 +1,9 @@
-import { ExcelParser } from '../package/excel_parser';
-import { ExcelReader } from '../package/excel_reader';
+import { ExcelParser } from '../package/excel_controller/excel_parser';
+import { ExcelReader } from '../package/excel_controller/excel_reader';
 
+/**
+ * Excel데이터와 API응답데이터의 일관성을 체크한다.
+ */
 export function Run() {
   const reader = new ExcelReader(
     '/Users/gimjaehyeong/Desktop/member_excel.xls'
@@ -8,12 +11,25 @@ export function Run() {
 
   const { workbook, sheetNames } = reader.getResult();
 
-  //   sheetNames.forEach((sheetName) => {
-  // solution
-  //   });
-
   sheetNames.forEach((sheetName) => {
-    const data = ExcelParser.GetParsedDataBySheetName(workbook, sheetName);
+    const data = ExcelParser.GetParsedDataBySheetName(
+      workbook,
+      sheetName,
+      (rawData: any[]) => {
+        const map = new Map<string, object>();
+
+        rawData.forEach((row: any) => {
+          const key = row['고유키(!)'];
+          const value = {
+            cnt: row['구매횟수(KRW)(!)'],
+            total_price: row['구매금액(KRW)(!)'],
+          };
+          map.set(key, value);
+        });
+
+        return map;
+      }
+    );
     console.log(data);
   });
 }
