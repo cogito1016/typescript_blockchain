81ab7a9f431e5cb87ec35c906541a3cfec2f6941
김재형 <cogito1016@naver.com>
2023-08-29 09:06:18 +0900

    feat: 엑셀,api데이터 비교로직

--- process/api_excel_checker/run.ts
+++ process/api_excel_checker/run.ts
@@ -16,7 +16,7 @@ export async function Run() {
 
   /**
    * Map(159) {
-    '멤버Code' => { cnt: '23', total_price: '567733' },
+    'membercode' => { cnt: '23', total_price: '567733' },
     ...
    */
   sheetNames.forEach((sheetName) => {
@@ -29,8 +29,8 @@ export async function Run() {
         rawData.forEach((row: any) => {
           const key = row['고유키(!)'];
           const value = {
-            cnt: row['구매횟수(KRW)(!)'],
-            total_price: row['구매금액(KRW)(!)'],
+            cnt: parseInt(row['구매횟수(KRW)(!)']),
+            total_price: parseInt(row['구매금액(KRW)(!)']),
           };
           map.set(key, value);
         });
@@ -42,23 +42,58 @@ export async function Run() {
     excelDataList = new Map([...excelDataList, ...data]);
   });
 
-  /**
-   * Map(100) {
-  '멤버Code' => { cnt: 23, total_price: 567733 },
-   */
-  const apiData = await ApiReader.CallMemberList(1, 100, (rawData: any[]) => {
-    const map = new Map<string, object>();
+  let offset = 1;
+  let excelDataLength = excelDataList.size;
+  let apiDataLength = 0;
+  let notMatchedDataCount = 0;
 
-    rawData.forEach((row: any) => {
-      const key = row['member_code'];
-      const value = {
-        cnt: row['order_count'],
-        total_price: row['order_total_price'],
-      };
-      map.set(key, value);
+  while (true) {
+    /**
+   * Map(100) {
+  'membercode' => { cnt: 23, total_price: 567733 },
+   */
+    const apiData = await ApiReader.CallMemberList(
+      offset,
+      100,
+      (rawData: any[]) => {
+        const map = new Map<string, object>();
+
+        rawData.forEach((row: any) => {
+          const key = row['member_code'];
+          const value = {
+            cnt: row['order_count'],
+            total_price: row['order_total_price'],
+          };
+          map.set(key, value);
+        });
+
+        return map;
+      }
+    );
+
+    if (apiData.size == 0) {
+      console.log('end');
+      break;
+    }
+
+    apiDataLength += apiData.size;
+
+    apiData.forEach((value: object, key: string) => {
+      if (excelDataList.has(key)) {
+        const excelValue: object | undefined = excelDataList.get(key);
+        if (excelValue) {
+          if (JSON.stringify(value) !== JSON.stringify(excelValue)) {
+            notMatchedDataCount++;
+            console.log(key, value, excelValue);
+          }
+        }
+      }
     });
 
-    return map;
-  });
-  console.log(apiData);
+    offset++;
+  } //while end
+
+  console.log('excelDataLength', excelDataLength);
+  console.log('apiDataLength', apiDataLength);
+  console.log('notMatchedDataCount', notMatchedDataCount);
 }
